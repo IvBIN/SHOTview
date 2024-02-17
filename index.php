ca03b16ed1e8f845f40eed5a0a3f54e46f779b3b
avilovmaksim5 <avilovmaksim5@mail.ru>
2024-02-17 11:12:50 +0400

    update 17/02/24

--- index.php
+++ index.php
@@ -1,74 +1,87 @@
 <?php
 /** @var string $error_message - Текст ошибки */
-
 session_start();
 require './config/db.php';
-$itemGroup = select('SELECT * FROM groups');
-
+$itemGroup = select('SELECT * FROM groups order by id');
+require './php/current_grS.php';
 ?>
 
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="style.css">
-        <script src="script.js" defer></script>
-        <title>Menu</title>
-    </head>
-    <body>
-        <div class="container">
-            <div class="menu_field">
-                <div class="keyboard_up">
-                    <a href="./views/add_groupeS.php" class="btn btn_create">Создать расписание</a>
-                    <a href="./views/edit_groupS.php" class="btn btn_edit">Редактировать расписание</a>
-                </div>
-                <div class="keyboard_down">
-                    <a href="./views/delete_groupS.php" class="btn btn_delete">Удалить расписание</a>
-<!--                    <a href="select.php" class="btn">Список</a>-->
-                </div>
-            </div>
-            <div class="current_schedule">
-                <span class="title_c_schedule">Текущее расписание</span>
-                <div class="current_data">
-                    <div class="name_group_cur">
-                        <span class="title_gc">Группа</span>
-                        <ul>
-                            <?php foreach ($itemGroup as $item): ?>
-                                <li class="group_c_schedule gr_name"><?php echo $item['name']?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                    <div class="days_group_cur">
-                        <span class="title_gc">Дни недели</span>
-                        <ul>
-                            <?php foreach ($itemGroup as $item): ?>
-                                <li class="group_c_schedule gr_day"><?php echo $item['days_of_week']?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                    <div class="message_group_cur">
-                        <span class="title_gc">Сообщения</span>
-                        <ul>
-                            <?php foreach ($itemGroup as $item): ?>
-                                <li class="group_c_schedule gr_message"><?php echo $item['message']?></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+    <title>Menu</title>
+</head>
+<body>
+<div class="container">
+    <div class="menu_field">
+        <div class="keyboard_up">
+            <a href="./views/add_groupeS.php" class="btn btn_create">Создать расписание</a>
+            <a href="./views/edit_groupS.php" class="btn btn_edit">Редактировать расписание</a>
+        </div>
+        <div class="keyboard_down">
+            <a href="./views/delete_groupS.php" class="btn btn_delete">Удалить расписание</a>
+            <!--                    <a href="select.php" class="btn">Список</a>-->
+        </div>
+    </div>
+    <div class="current_schedule">
+        <span class="title_c_schedule">Текущее расписание</span>
+        <div class="current_data">
+            <table style="border: 1px solid white">
+                <tr>
+                    <th>Группа</th>
+                    <th>Дни недели</th>
+                    <th>Сообщения</th>
+                </tr>
+                <?php foreach ($itemGroup as $key=>$item) : ?>
+                    <tr>
+                        <td><?= $item['name'] ?></td>
+                        <td><?=!empty($finalyDaysGroup[$key]) ? implode(", ", $finalyDaysGroup[$key]) : "" ?></td>
+                        <td><?= $item['message'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+<!--            <div class="name_group_cur">-->
+<!--                <span class="title_gc">Группа</span>-->
+<!--                <ul>-->
+<!--                    --><?php //foreach ($itemGroup as $item): ?>
+<!--                        <li class="group_c_schedule gr_name">--><?php //echo $item['name'] ?><!--</li>-->
+<!--                    --><?php //endforeach; ?>
+<!--                </ul>-->
+<!--            </div>-->
+<!--            <div class="days_group_cur">-->
+<!--                <span class="title_gc">Дни недели</span>-->
+<!--                <ul>-->
+<!--                    --><?php //foreach ($itemGroup as $item): ?>
+<!--                        <li class="group_c_schedule gr_day">--><?php //echo $item['days_of_week'] ?><!--</li>-->
+<!--                    --><?php //endforeach; ?>
+<!--                </ul>-->
+<!--            </div>-->
+<!--            <div class="message_group_cur">-->
+<!--                <span class="title_gc">Сообщения</span>-->
+<!--                <ul>-->
+<!--                    --><?php //foreach ($itemGroup as $item): ?>
+<!--                        <li class="group_c_schedule gr_message">--><?php //echo $item['message'] ?><!--</li>-->
+<!--                    --><?php //endforeach; ?>
+<!--                </ul>-->
+<!--            </div>-->
 
 
-                </div>
-
-
-            </div>
-
-            <div class="convert_field">
-                <a href="./php/current_grS.php" class="converter">Преобразование в дни недели</a>
-            </div>
         </div>
 
-    </body>
+
+    </div>
+
+<!--    <div class="convert_field">-->
+<!--        <a href="./php/current_grS.php" class="converter">Преобразование в дни недели</a>-->
+<!--    </div>-->
+</div>
+
+</body>
 </html>
 
--- php/current_grS.php
+++ php/current_grS.php
@@ -1,15 +1,7 @@
 <?php
-/** @var string $error_message - Текст ошибки */
-session_start();
-require '../config/db.php';
-$itemGroup = select('SELECT * FROM groups');
-
-//var_dump($_POST);
-
+/** @var string $itemGroup - all group */
 //преобразование цифр в дни недели (from database)
 //передача обработанных данных на главную страницу (index.php)
-
-
 $transDayCur = [
     '0' => "Понедельник",
     '1' => "Вторник",
@@ -19,57 +11,21 @@ $transDayCur = [
     '5' => "Суббота",
     '6' => "Воскресенье",
 ];
-//var_dump($transDayCur);
-
-$dayGroupCurr = [];
-
+$finalyDaysGroup = [];
 foreach ($itemGroup as $item) {
-//    $dayCurr = exlode(", ", $item['days_of_week']);
-//    var_dump($dayCurr);
+    $dayGroupCurr = [];
 
-  if (isset($transDayCur[$item['days_of_week']])){
-      $dayGroupCurr[]= $item['days_of_week'];
-  }
-    var_dump($item['days_of_week']);
+    if (empty($item['days_of_week'])) {
+        $finalyDaysGroup[] = null;
+        continue;
+    }
+
+    $dayCurr = explode(", ", $item['days_of_week']);
+    foreach ($dayCurr as $day) {
+        if (isset($transDayCur[$day])) {
+            $dayGroupCurr[] = $transDayCur[$day];
+        }
+    }
+
+    $finalyDaysGroup[] = $dayGroupCurr;
 }
-
-
-var_dump($dayGroupCurr);
-
-//$userDay = explode(", ", $_POST["day_of_w"]);
-//$finalyDayCur = [];
-//
-//foreach ($userDay as $day) {
-////    var_dump($day);
-//    if (isset($transDay[$day])) {
-//        $finalyDay[] = $transDay[$day];
-//    }
-//}
-//var_dump($finalyDay);
-
-//$finalyDayStr = implode(", ", $finalyDay);
-//var_dump($finalyDayStr);
-
-//$nameG = $_POST["group"];
-//var_dump($nameG);
-
-//$id_group = select("SELECT id FROM groups WHERE name = :nameG", ['nameG' => $nameG]);
-//$idG = $id_group[0]['id'];
-
-//var_dump($idG);
-
-//$messageG = $_POST["message"];
-//var_dump($messageG);
-
-
-//$updateDB = update("UPDATE groups SET days_of_week = :days_of_week,
-//                      message = :message WHERE id = :id",
-//    [
-//        'days_of_week' => $finalyDayStr,
-//        'message' => $messageG,
-//        'id' => $idG,
-//    ]
-//);
-
-//var_dump($updateDB);
-
